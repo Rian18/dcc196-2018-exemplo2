380c663ffc9373d4caa35647add557349ee5de2c
Rian Alves <rianalves2009.ra@gmail.com>
2018-08-29 20:14:28 -0300

    Adiciona notificações para o Estado do ProfessorActivity

--- app/src/main/java/dcc196/ufjf/br/exemplo02/MainActivity.java
+++ app/src/main/java/dcc196/ufjf/br/exemplo02/MainActivity.java
@@ -5,21 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnProfessor;
+    public static final String PESSOA_NOME = "nome";
+    private Button btnProfessor;
+    private EditText edtNome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnProfessor = findViewById(R.id.btn_Professor);
+        edtNome = findViewById(R.id.edt_nome);
 
         btnProfessor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,ProfessorActivity.class);
+                intent.putExtra(PESSOA_NOME,edtNome.getText().toString());
+
                 startActivity(intent);
-                
+
             }
         });
     }
